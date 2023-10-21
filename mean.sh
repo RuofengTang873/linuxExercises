6258bd0fbdc29fb424dd754cd8a8b68c69540a87
Ruofeng Tang <rtang64@wisc.edu>
2023-10-20 21:20:38 -0500

    final changes

--- mean.sh
+++ mean.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+echo "usage: ./$0 <column> [file.csv], that reads the column specified by <column> (a number) from the comma-separated-values file (with header) specified by [file.csv] (or from stdin if no [file.csv] is specified) and writes its mean." 1>&2
+if [ -z "$2" ]; then 
+    file='/dev/stdin'
+else 
+    file=$2
+fi
+cut -d "," -f $1 $file | tail -n +2 | { sum=0; count=0; while read n; do sum=$(($sum + $n)); count=$(($count + 1)); done; echo $sum / $count; } | bc -l
--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+echo "usage: ./$0 <dir> <n> that removes all files in directory dir larger than <n> bytes." 1>&2
+cd $1
+find . -type f -size -$2\c | while read n; do rm -f $n; done
